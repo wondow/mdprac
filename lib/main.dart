075a0edc2490834691d81854e1fe7e61a377a367
wondow <debbiegitahi@gmail.com>
2026-03-10 18:08:25 +0300

    feat: initialize login

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
           backgroundColor: Colors.cyanAccent,
           title: Text(
@@ -13,31 +14,89 @@ void main() {
           ),
           centerTitle: true,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+        body: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
 
-          children: [
-            Text(
-              "My Application",
-              style: TextStyle(
-                color: Colors.deepPurple,
-                fontSize: 37,
-                fontWeight: FontWeight.bold,
+                children: [
+                  Text(
+                    "Winx",
+                    style: TextStyle(
+                      color: Colors.deepPurple,
+                      fontSize: 37,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/images/winx.png',
+                    height: 200,
+                    width: 200,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Enter Username",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        prefixIcon: Icon(Icons.gradient_rounded),
+                      ),
+                    ),
+                  ),
+                  Text("Enter password"),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Email or phone number",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        prefixIcon: Icon(Icons.lock),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  MaterialButton(
+                    onPressed: () {},
+                    color: Colors.white,
+                    textColor: Colors.deepOrangeAccent,
+                    child: Text("Login"),
+                  ),
+
+                  Container(
+                    height: 50,
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(color: Colors.blue),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(color: Colors.blue, fontSize: 14),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Text("Enter username"),
-            TextField(),
-            Text("Enter password"),
-            TextField(),
-            SizedBox(height: 30),
-            MaterialButton(
-              onPressed: () {},
-              color: Colors.white,
-              textColor: Colors.deepOrangeAccent,
-              child: Text("Login"),
-            ),
-          ],
+          ),
         ),
       ),
     ),
